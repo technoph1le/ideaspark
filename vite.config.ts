3d65262985d1d47c7a425b9695be5b5b031e8684
Technophile <dostonnabotov@gmail.com>
2025-06-04 02:37:04 +0300

    Fixed import error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import VitePluginSitemap from "vite-plugin-sitemap";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginSitemap()],
+  plugins: [react(), Sitemap()],
 });
