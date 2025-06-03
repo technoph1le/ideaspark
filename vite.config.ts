a5664c831b8cb5e502a4230579b750f48596e3cb
Technophile <dostonnabotov@gmail.com>
2025-06-04 02:31:43 +0300

    Add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import VitePluginSitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/ideaspark/",
+  plugins: [react(), VitePluginSitemap()],
 });
